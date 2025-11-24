d63d0ad444ada7e50856b37480a3505ded57fc71
gott150899 <gott150899@gmail.com>
2025-11-24 22:13:51 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SwUpdate, VersionReadyEvent } from '@angular/service-worker';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +11,13 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'angular-pwa-test';
+
+  constructor(){
+    const swUpdate = inject(SwUpdate);
+    swUpdate.versionUpdates
+      .pipe(filter((evt): evt is VersionReadyEvent => evt.type === 'VERSION_READY'))
+      .subscribe((evt) => {
+        alert('Có phiên bản mới. Vui lòng reload để làm mới')
+      });
+  }
 }
